03a3fde9035bacea6678020a1a38ca3c1ba191d7
jakubsokol <kubas.sokol@gmail.com>
2024-07-28 15:21:19 +0200

    adding counter

--- main.py
+++ main.py
@@ -27,8 +27,6 @@ async def button_click():
 # async def button_click():
 #     return JSONResponse(content={"message": "Button clicked!"})
 
-
-
 if __name__=="__main__":
     uvicorn.run(app, host="127.0.0.1",port=8000)
 
